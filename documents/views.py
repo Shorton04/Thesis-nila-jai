7222b639f2df4f16bea6c8bf6024aa54f2e29a3a
Shorton04 <120240879+Shorton04@users.noreply.github.com>
2025-01-27 10:05:03 +0800

    httpsresponse

--- documents/views.py
+++ documents/views.py
@@ -1,7 +1,7 @@
 # documents/views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse, HttpResponseBadRequest
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
