238eece005f1d693f40fd3101123f9594163790d
Shorton04 <120240879+Shorton04@users.noreply.github.com>
2025-04-02 12:31:29 +0800

    Initial commit for Heroku deployment

--- business_permit_system/settings.py
+++ business_permit_system/settings.py
@@ -6,7 +6,7 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
+SECRET_KEY = config('SECRET_KEY', default='jaijerichoathaniahn123')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
@@ -174,8 +174,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='rmarcdexter@gmail.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='Daboy*123')
 
 # Custom User Model
 AUTH_USER_MODEL = 'accounts.CustomUser'
